ee5ec094c158582be0a2018aec69b66db285d585
USER <USER@1.209.180.136>
2024-07-26 12:04:19 +0900

    <2024.07.26> 실습

--- jdbc_test/src/jdbc_00/jdbc_test.java
+++ jdbc_test/src/jdbc_00/jdbc_test.java
@@ -5,13 +5,14 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class jdbc_test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-
 		//	SQL연결
 		final String driver = "org.mariadb.jdbc.Driver";
 		final String db_ip = "localhost"; /*데이터베이스 URL*/
@@ -19,14 +20,17 @@ public class jdbc_test {
 		final String db_name = "student_test"; /*DB명*/
 		final String db_url =
 				"jdbc:mariadb://"+db_ip+":"+db_port+"/"+db_name;
+		/*여기까지 데이터베이스 연결하는 것으로 이름,명칭,포트번호 등을 통해서 불러오기*/
+
 
 		//	connection 	연결
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
-
+		/*여기는 연결하고 쿼리실행 후 결과값 담기까지가 */
 		try {
 			Class.forName(driver);
+			/*접속*/
 			conn = DriverManager.getConnection(db_url, "root", "4623");
 			if(conn != null) {
 				System.out.println("DB 접속성공");
@@ -39,33 +43,56 @@ public class jdbc_test {
 		} catch(SQLException e) {
 			System.out.println("DB접속 실패");
 			e.printStackTrace();
+			/*데이터베이스 연결 실패들은 모두 SQLException에 들어감 */
 		}
 
 		try {
 
 			String sql ="SELECT school_id, school_name, school_area " /*띄어쓰기 필수*/
-					+ "FROM tb_school_info "
-					+"WHERE school_id = 1";
+					+ "FROM tb_school_info ";
+
+			//	    +"WHERE school_id = 1";
+
 			System.out.println(sql);
 			pstmt = conn.prepareStatement(sql);
 
-
 			rs = pstmt.executeQuery();
-			int schoolId = 0;
-			String schoolName = null;
-			String schoolArea = null;
+			List<HashMap<String, Object>> list = new ArrayList();
+
+			/*prepareStatement = sql ="SELECT school_id, school_name, school_area " 
+			 * + "FROM tb_school_info "
+					   				+"WHERE school_id = 1";
+					   담겨있음*/
+
+			//			int schoolId = 0;
+			//			String schoolName = null;
+			//			String schoolArea = null;
+
 
 			while (rs.next()) {
-				schoolId = rs.getInt(1);
-				schoolName = rs.getString("school_name");
-				schoolArea = rs.getString(3);
+				HashMap<String, Object> rsMap = new HashMap<String, Object>();
+				rsMap.put("schoolId", rs.getInt(1));
+				rsMap.put("schoolName", rs.getString(2));
+				rsMap.put("schoolArea", rs.getString(3));
+				list.add(rsMap);
+
+				//				schoolId = rs.getInt(1);
+				//				schoolName = rs.getString("school_name");
+				//				schoolArea = rs.getString(3);
 			}
-			System.out.println("schoolId :"+schoolId);
-			System.out.println("schoolId :"+schoolName);
-			System.out.println("schoolId :"+schoolArea);
+			//			System.out.println("schoolId :"+schoolId);
+			//			System.out.println("schoolName :"+schoolName);
+			//			System.out.println("schoolArea :"+schoolArea);
+			System.out.println("학교ID / t학교이름 / t학교지역");
+			for(int i = 0; i<list.size(); i++) {
+				System.out.println(list.get(i).get("schoolId").toString()+"\t"
+						+list.get(i).get("schoolName").toString()+"\t"
+						+list.get(i).get("schoolArea").toString());
+
+			}
 		}catch (SQLException e){
-			//
-			System.out.println("error :"+e);
+			//TODO handle exception
+			System.out.println("error : "+e);
 		}finally {
 
 			try {
@@ -82,7 +109,7 @@ public class jdbc_test {
 
 			} catch(SQLException e) {
 				e.printStackTrace();
-				
+
 			}
 		}
 	}
