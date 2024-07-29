d520fa8ffdeb5ffe66588c4225c9c81f5c18ec51
USER <USER@1.209.180.136>
2024-07-29 12:45:41 +0900

    <2024.07.29> book,최초커밋 javaSQL

---
+++ jdbc_test/src/book/BookController.java
@@ -0,0 +1,13 @@
+package book;
+
+public class BookController {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub		
+		BookService bookService = new BookServiceImpl();
+		
+		bookService.startProgram();
+
+	}
+
+}
---
+++ jdbc_test/src/book/BookService.java
@@ -0,0 +1,9 @@
+package book;
+
+public interface BookService {
+	
+	public void startProgram();
+	
+	public int printMenu();
+	
+}
